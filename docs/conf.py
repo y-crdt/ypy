9da503d8082c7800671a3097e8dab99109c498ce
waidhoferj <waidhoferj@gmail.com>
2022-07-20 22:42:49 -0700

    adjusted versions

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2022, Kevin Jahns, Bartosz Sypytkowski, John Waidhofer"
 author = "Kevin Jahns, Bartosz Sypytkowski, John Waidhofer"
 
 # The full version, including alpha/beta/rc tags
-release = "0.5.4"
+release = "0.5.3"
 
 
 # -- General configuration ---------------------------------------------------
