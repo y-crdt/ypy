ce6329a72cd6225bc0c37653cedd569f22d604cd
waidhoferj <waidhoferj@gmail.com>
2022-10-03 23:05:35 -0700

    basic slow

--- examples/database-adapter/client.py
+++ examples/database-adapter/client.py
@@ -1,11 +1,8 @@
 import y_py as Y
 import asyncio
-from tkinter import Tk, Text, BOTH, INSERT
+from tkinter import Tk, Text, BOTH, INSERT, END
 from websockets import connect
 from ypy_websocket import WebsocketProvider 
-import asynctkinter as at
-from random import randint
-at.patch_unbind()
 
 
 class TextEditor:
@@ -18,63 +15,54 @@ class TextEditor:
     def __init__(self, id:int=0):
         self.doc = Y.YDoc(id)
         self.text = self.doc.get_text("content")
-        self.text.observe(print)
-
-        
-
-    def render(self):
+        self.text.observe(self.render_text)
         # root
         self.root = Tk()
         self.textbox = Text(self.root)
-        # self.textbox.unbind("<KeyPress>")
+        self.textbox.bind("<KeyPress>", self.on_entry)
+        self.textbox.bind("<KeyPress-BackSpace>", self.on_delete)
         self.textbox.pack(expand=True, fill=BOTH)
         self.root.minsize(300, 300)
         self.root.title("Text Editor")
-        self.register_event_handlers()
-        self.root.mainloop()
+
+        
+
+    async def render(self):
+        while True:
+            self.root.update()
+            await asyncio.sleep(0.01)
+
+
+    def render_text(self,e: Y.YTextEvent):
+        i = self.cursor_index()
+        self.textbox.delete("1.0", END)
+        self.textbox.insert(END, str(self.text))
+        self.textbox.mark_set("insert", f"1.{i}")
 
     def cursor_index(self) -> int:
-        line, chr = [ int(s) for s in self.textbox.index(INSERT).split(".") ]
-        return chr
+        line, col = [ int(s) for s in self.textbox.index(INSERT).split(".") ]
+        return col
 
-        
 
-        
-    
-    def register_event_handlers(self):
-        handlers = [self.on_click, self.on_delete, self.on_entry]
-        for handler in handlers:
-            at.start(handler())
-
-    async def on_entry(self):
-        while True:
-            e = await at.event(self.textbox, "<KeyPress>")
-            if e.char == "BackSpace" and len(e.char) == 1:
-                continue
-            with self.doc.begin_transaction() as txn:
-                index = self.cursor_index()
-                self.text.insert(txn,index,e.char)
+    def on_entry(self, e):
+        print(e)
+        if e.char == "BackSpace" and len(e.char) == 1:
+            return
+        char = "\n" if e.keysym == "Return" else e.char
+        with self.doc.begin_transaction() as txn:
+            index = self.cursor_index()
+            print(index)
+            self.text.insert(txn,index,char)
 
 
 
-    async def on_delete(self):
-        while True:
-            await at.event(self.textbox, '<KeyPress-BackSpace>')
-            with self.doc.begin_transaction() as txn:
-                del_index = self.cursor_index() - 1
-                if del_index < 0:
-                    continue
-                self.text.delete(txn, del_index)
+    def on_delete(self, e):
+        with self.doc.begin_transaction() as txn:
+            del_index = self.cursor_index() - 1
+            if del_index < 0:
+                return
+            self.text.delete(txn, del_index)
             
-
-    
-    async def on_click(self):
-        while True:
-            await at.event(self.textbox, '<Button>')
-            # do something with cursor
-
-
-
     async def connect_ws(self, websocket_url:str = "ws://localhost:1234/my-roomname"):
         websocket = await connect(websocket_url)
         self.websocket_client = WebsocketProvider(self.doc, websocket)
@@ -82,8 +70,8 @@ class TextEditor:
 
 async def main():
     te = TextEditor()
-    at.start(te.connect_ws())
-    te.render()
+    await te.connect_ws()
+    await te.render()
 
 if __name__ == "__main__":
     asyncio.run(main())
