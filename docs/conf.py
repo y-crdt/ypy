e3202b7cebd79d955f486a0bc2266c35ba1f7c98
waidhoferj <waidhoferj@gmail.com>
2022-11-27 23:33:21 -0800

    Incremented release

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2022, Kevin Jahns, Bartosz Sypytkowski, John Waidhofer"
 author = "Kevin Jahns, Bartosz Sypytkowski, John Waidhofer"
 
 # The full version, including alpha/beta/rc tags
-release = "0.5.3"
+release = "0.5.5"
 
 
 # -- General configuration ---------------------------------------------------
